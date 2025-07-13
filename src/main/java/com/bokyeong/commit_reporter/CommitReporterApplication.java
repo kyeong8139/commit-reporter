f416b1123c9f0e9495e1ac7359ae087b4ec02fd6
leebokyeong <kyeong8139@gmail.com>
2025-07-13 21:41:31 +0900

    project init

---
+++ src/main/java/com/bokyeong/commit_reporter/CommitReporterApplication.java
@@ -0,0 +1,13 @@
+package com.bokyeong.commit_reporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitReporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bokyeong/commit_reporter/CommitReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bokyeong.commit_reporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
